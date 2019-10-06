cd31403693a7dd15596da116646e7899160b80ba
Tom <thomas.hofbauef@gmail.com>
2019-10-06 12:33:19 +0200

    added Filewriter and basic Ui

--- src/ich/ich/UI.java
+++ src/ich/ich/UI.java
@@ -2,8 +2,11 @@ package ich.ich;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
 
 public class UI extends JFrame {
+
+    FileWriter writer;
 
     GridLayout tilelayout;
     BorderLayout outerlayout;
@@ -13,7 +16,19 @@ public class UI extends JFrame {
 
     JButton[][] buttons;
 
+    JButton btntree;
+    JButton btnwall;
+    JButton btnclear;
+    JButton btnsave;
+
+    JTextField area;
+
+
+
+
     char[][] level;
+
+    char active;
 
     int buttonsize;
     int rows;
@@ -25,6 +40,8 @@ public class UI extends JFrame {
         this.columns = columns;
         this.buttonsize = 32;
         this.level = new char[rows][columns];
+        this.active = ' ';
+       // this.writer = new FileWriter();
 
         this.buttonspanel = new JPanel();
 
@@ -47,6 +64,11 @@ public class UI extends JFrame {
         makeButtons();
 
         controlpanel = new JPanel();
+    //    controlpanel.setVisible(true);
+
+        addControl();
+
+
         this.add(controlpanel, BorderLayout.SOUTH);
 
 
@@ -54,15 +76,84 @@ public class UI extends JFrame {
         this.setVisible(true);
     }
 
+    private String activeToPath(char active){
+        String path = "images/";
+        switch (active){
+            case 'b':
+                path = path + "tree.jpg";
+                break;
+            case 'm':
+                path = path + "wall.jpg";
+                break;
+            case ' ':
+                path = path + "empty.jpg";
+                break;
+            default:
+                path = path+ "empty.jpg";
+
+        }
+        return path;
+    }
+
     private void makeButtons(){
         for(int i = 0; i < buttons.length; i++){
             for(int j = 0; j < buttons[0].length;j++){
+                final int x = i;
+                final int y = j;
                 buttons[i][j] = new JButton();
                 buttons[i][j].setVisible(true);
+                buttons[i][j].addActionListener(e -> {
+                    buttons[x][y].setIcon(new ImageIcon(activeToPath(this.active)));
+                    level[x][y] = this.active;
+                });
                /* buttons[i][j].setBorder(BorderFactory.createEmptyBorder());
                 buttons[i][j].setContentAreaFilled(false);*/
                this.buttonspanel.add( buttons[i][j]);
             }
         }
+    }
+
+    private void addControl(){
+        btntree = new JButton();
+        btntree.setIcon(new ImageIcon("images/tree.jpg"));
+        btntree.addActionListener(e -> this.active = 'b');
+        controlpanel.add(btntree);
+
+        btnwall = new JButton();
+        btnwall.setIcon(new ImageIcon("images/wall.jpg"));
+        btnwall.addActionListener(e -> this.active = 'm');
+        controlpanel.add(btnwall);
+
+        btnclear = new JButton("Remove");
+        btnclear.addActionListener(e -> this.active = ' ');
+        controlpanel.add(btnclear);
+
+        area = new JTextField(20);
+
+        controlpanel.add(area);
+
+        btnsave = new JButton("Save");
+        btnsave.addActionListener(e -> {for(char[] item: level){
+            for(char charitem: item){
+                if(charitem == '\u0000'){
+                    charitem = ' ';
+                }
+                System.out.print(charitem);
+            }
+            System.out.println("");
+
+            }
+        });
+        controlpanel.add(btnsave);
+
+    }
+
+    public ArrayList<String> getLevel(){
+        ArrayList<String> temp = new ArrayList<>();
+        for(char[] item: level){
+            temp.add(new String(item));
+        }
+
+        return temp;
     }
 }
